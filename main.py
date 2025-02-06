572318afff81a8f4f22b904a8a6cfff923b85333
Diligence Dev <diligence.dev@web.de>
2025-02-06 21:59:08 +0100

    cleanup and formatting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, render_template, redirect, url_for
 import random
-import requests
 from collections import defaultdict, OrderedDict
 from statistics import mean
 import re
@@ -12,25 +11,25 @@ port = 5000
 app = Flask(__name__)
 
 # Dictionary to store state for each event
-events = defaultdict(lambda: {
-    "x": [make_round_result([])],
-    "previous_states": []
-})
+events = defaultdict(
+    lambda: {"x": [make_round_result(["a", "b", "c", "d"])], "previous_states": []}
+)
+
 
 def save_state(event_id):
-    global events
     events[event_id]["previous_states"].append(copy.deepcopy(events[event_id]["x"]))
 
+
 def make_round_result(players):
-    need_bye = len(players) % 2 == 1
-    if need_bye:
+    if len(players) % 2 == 1:
         players.append("bye")
     round_results = OrderedDict(
         ((players[i], players[i + 1]), (-1, -1)) for i in range(0, len(players), 2)
     )
-    if need_bye:
-        round_results[list(round_results.keys())[-1]] = (2, 0)
+    if players[-1] == "bye":
+        round_results[(players[-2], "bye")] = (2, 0)
     return round_results
+
 
 def get_players(event_id):
     x = events[event_id]["x"]
@@ -38,11 +37,14 @@ def get_players(event_id):
         p if p != "bye" else o for o, p in x[0].keys()
     ]
 
+
 def get_pairing(event_id):
     return list(events[event_id]["x"][-1].keys())
 
+
 def get_pairing_with_score(event_id):
     return events[event_id]["x"][-1].items()
+
 
 def calculate_standings(event_id):
     x = events[event_id]["x"]
@@ -102,15 +104,14 @@ def calculate_standings(event_id):
     standings.sort(key=lambda x: (-x["points"], -x["omw"]))
     return standings
 
+
 def new_round(event_id):
-    global events
     save_state(event_id)
     x = events[event_id]["x"]
-    pairings = []
 
     # Swiss pairing using maximum weight matching
     standings = calculate_standings(event_id)
-    players = [p["name"] for p in standings] # in standings order
+    players = [p["name"] for p in standings]  # in standings order
     pairing_history = {
         frozenset(match) for round_results in x for match in round_results
     }
@@ -122,15 +123,15 @@ def new_round(event_id):
 
     # Create a graph
     G = nx.Graph()
-
-    # Add nodes
     G.add_nodes_from(players)
 
     # Add edges with weights based on points
     for i, p1 in enumerate(players):
         for p2 in players[i + 1 :]:
             if frozenset({p1, p2}) not in pairing_history:
-                score_diff = abs(standings[i]["points"] - standings[players.index(p2)]["points"])
+                score_diff = abs(
+                    standings[i]["points"] - standings[players.index(p2)]["points"]
+                )
                 G.add_edge(p1, p2, weight=-score_diff)
 
     # Find the maximum weight matching
@@ -146,6 +147,7 @@ def new_round(event_id):
         )
     )
 
+
 # Routes
 @app.route("/<int:event_id>/")
 def index(event_id):
@@ -156,26 +158,31 @@ def index(event_id):
         pairing_with_score=get_pairing_with_score(event_id),
         standings=calculate_standings(event_id),
         round_number=len(events[event_id]["x"]),
-        event_id=event_id
+        event_id=event_id,
     )
+
 
 @app.route("/<int:event_id>/qr", methods=["POST"])
 def qr(event_id):
     url = request.form.get("url")
-    return render_template("qr.html", url=f"{url}/{event_id}/new_player", players=get_players(event_id))
+    return render_template(
+        "qr.html", url=f"{url}/{event_id}/new_player", players=get_players(event_id)
+    )
+
 
 @app.route("/<int:event_id>/shuffle_seatings", methods=["POST"])
 def shuffle_seatings(event_id):
-    global events
     save_state(event_id)
     x = events[event_id]["x"]
     assert len(x) == 1
-    x[0] = make_round_result(random.sample(get_players(event_id), len(get_players(event_id))))
+    x[0] = make_round_result(
+        random.sample(get_players(event_id), len(get_players(event_id)))
+    )
     return redirect(url_for("index", event_id=event_id))
+
 
 @app.route("/<int:event_id>/submit_results", methods=["POST"])
 def submit_results(event_id):
-    global events
     save_state(event_id)
     for i, (p1, p2) in enumerate(get_pairing(event_id)):
         p1_games_won = int(request.form.get(f"p1_games_won_{i+1}"))
@@ -187,24 +194,33 @@ def submit_results(event_id):
     new_round(event_id)
     return redirect(url_for("index", event_id=event_id))
 
+
 # player interface
 @app.route("/<int:event_id>/new_player")
 def new_player(event_id):
     name = request.args.get("name")
     error_message = None if not name else f"Player '{name}' already exists"
-    return render_template("new_player.html", error_message=error_message, players=get_players(event_id), event_id=event_id)
+    return render_template(
+        "new_player.html",
+        error_message=error_message,
+        players=get_players(event_id),
+        event_id=event_id,
+    )
+
 
 @app.route("/<int:event_id>/add_player", methods=["POST"])
 def add_player(event_id):
-    global events
     save_state(event_id)
     name = request.form.get("name")
     name = re.sub(r"[^A-Za-z]", "_", name)
     if name not in get_players(event_id) and len(events[event_id]["x"]) == 1:
         events[event_id]["x"][0] = make_round_result(get_players(event_id) + [name])
-        return redirect(url_for("draft_seating_highlight", event_id=event_id, name=name))
+        return redirect(
+            url_for("draft_seating_highlight", event_id=event_id, name=name)
+        )
     else:
         return redirect(url_for("new_player", event_id=event_id, name=name))
+
 
 @app.route("/<int:event_id>/player/<name>")
 def player(event_id, name):
@@ -230,12 +246,12 @@ def player(event_id, name):
         s2=match[3],
         name=name,
         standings=calculate_standings(event_id),
-        event_id=event_id
+        event_id=event_id,
     )
+
 
 @app.route("/<int:event_id>/submit_result", methods=["POST"])
 def submit_result(event_id):
-    global events
     save_state(event_id)
     p1 = request.form.get(f"p1")
     p2 = request.form.get(f"p2")
@@ -253,22 +269,35 @@ def submit_result(event_id):
 
     return redirect(url_for("player", event_id=event_id, name=submitting_player))
 
+
 @app.route("/<int:event_id>/draft_seating")
 def draft_seating(event_id):
-    return render_template("draft_seating.html", players=get_players(event_id), highlight=None, event_id=event_id)
+    return render_template(
+        "draft_seating.html",
+        players=get_players(event_id),
+        highlight=None,
+        event_id=event_id,
+    )
+
 
 @app.route("/<int:event_id>/draft_seating/<name>")
 def draft_seating_highlight(event_id, name):
     if name not in get_players(event_id):
         return redirect(url_for("draft_seating", event_id=event_id))
-    return render_template("draft_seating.html", players=get_players(event_id), highlight=name, event_id=event_id)
+    return render_template(
+        "draft_seating.html",
+        players=get_players(event_id),
+        highlight=name,
+        event_id=event_id,
+    )
+
 
 @app.route("/<int:event_id>/undo")
 def undo(event_id):
-    global events
     if events[event_id]["previous_states"]:
         events[event_id]["x"] = events[event_id]["previous_states"].pop()
     return redirect(url_for("index", event_id=event_id))
+
 
 if __name__ == "__main__":
     app.run(host="localhost", port=port)
